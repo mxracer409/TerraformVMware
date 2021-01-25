82b706c364fab64060a13ec43a838f8c5fb887e3
Justin West <mxracer409@hotmail.com>
2021-01-25 13:43:51 -0800

    comment out

--- modular/modules/vm/outputs.tf
+++ modular/modules/vm/outputs.tf
@@ -1,10 +1,10 @@
 
-output "machineID" {
-  #description = "this is the Machine ID"
-  value = "${vsphere_virtual_machine.Clone1.id}"
-}
-
-output "machine_IP" {
-  description = "VM IP"
-  value = "${vsphere_virtual_machine.Clone1.guest_ip_addresses}"
-}
+#output "machineID" {
+#  #description = "this is the Machine ID"
+#  value = "${vsphere_virtual_machine.Clone1.id}"
+#}
+#
+#output "machine_IP" {
+#  description = "VM IP"
+#  value = "${vsphere_virtual_machine.Clone1.guest_ip_addresses}"
+#}
