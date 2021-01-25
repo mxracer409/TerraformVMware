d9b3f3eaac2ba5c00118a7ad5549b15dcafb9246
Justin West <mxracer409@hotmail.com>
2021-01-25 12:59:28 -0800

    correcting module variable references

--- modular/main.tf
+++ modular/main.tf
@@ -33,45 +33,18 @@ provider "vsphere" {
 
 module "datagather_module" {
     #provider = vsphere
-    source = "/modules/datagather/datagather.tf"
+    source = "./modules/datagather"
     
 }
 
 module "makevm_module" {
     #provider = vsphere
-    source = "/modules/vm/makevm.tf"
-    vsphere_datacenter = "${}"
-    vsphere_datastore = "${}"
-    vsphere_compute_cluster = "${}"
-    vsphere_network = "${}"
-    vsphere_template = "${}"
+    source = "./modules/vm"
+    vsphere_datacenter = "${module.datagather_module.vsphere_datacenter.id}"
+    vsphere_datastore = "${module.datagather_module.vsphere_datastore.id}"
+    vsphere_compute_cluster = "${module.datagather_module.vsphere_compute_cluster.id}"
+    vsphere_network = "${module.datagather_module.vsphere_network.id}"
+    #vsphere_template = "${}"
     
     
 }
-
-
-/*
-output "vsphere_datacenter" {
-    value = "${vsphere_datacenter.dc.id}"
-}
-
-output "vsphere_datastore" {
-    value = "${vsphere_datacenter.datastore.id}"
-}
-
-output "vsphere_compute_cluster" {
-    value = "${vsphere_datacenter.cluster.id}"
-}
-
-#output "vsphere_resource_pool" {
-#    value = "${vsphere_datacenter.pool.id}"
-#}
-
-output "vsphere_network" {
-    value = "${vsphere_datacenter.network.id}"
-}
-
-output "vsphere_template" {
-  value = "${vsphere_datacenter.template.id}"
-}
-*/
