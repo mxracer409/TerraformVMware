898ede4f054a6ad9bc8b212b277dcaecd4c53618
Justin West <mxracer409@hotmail.com>
2021-01-25 13:13:21 -0800

    updating for non template deploy

--- modular/modules/vm/makevm.tf
+++ modular/modules/vm/makevm.tf
@@ -1,3 +1,45 @@
+######################################
+### Build a Unbuntu VM from an ISO ###
+######################################
+
+resource "vsphere_virtual_machine" "vm" {
+  name              = "Ubuntu_TF"
+  resource_pool_id  = "${var.vsphere_compute_cluster.cluster.resource_pool_id}"
+  #resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id      = "${var.vsphere_datastore.datastore.id}"
+
+  num_cpus = 1
+  memory   = 1024
+  guest_id = "ubuntu64Guest"
+  #this is the OS identification when creating a VM - look this up at https://code.vmware.com/apis/358/doc/vim.vm.GuestOsDescriptor.GuestOsIdentifier.html
+  wait_for_guest_net_timeout = 0
+  scsi_type = "lsilogic-sas"
+
+  network_interface {
+    network_id = "${var.vsphere_network.network.id}"
+  }
+
+  disk {
+    label  = "Ubuntu"
+    size  = 25
+    #thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+    thin_provisioned = true
+    unit_number = 0
+  }
+
+  cdrom {
+    datastore_id = "${var.vsphere_datastore.datastore.id}" #the data store the ISO is located
+    #path = "vStorage1/Ubuntu/ubuntu-16.04.iso"
+    path = "Ubuntu/ubuntu-16.04.iso"
+  }
+}
+
+
+
+
+
+
+/*
 #################################
 #### clone a VM from Template ###
 #################################
@@ -43,3 +85,4 @@ resource "vsphere_virtual_machine" "Clone1" {
     }
   }
 }
+*/
